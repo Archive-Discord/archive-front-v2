6f77227b528772590af5f387c481dc3d215ad9d8
kiss8981 <kdh89811@gmail.com>
2022-03-31 00:13:08 +0900

    fix: head  users page

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -9,25 +9,27 @@ import dynamic from 'next/dynamic'
 import ErrorPage from '@components/ErrorPage'
 import { userAvaterLink } from '@utils/Tools'
 import DateFormet from '@utils/Date'
+import { isBuffer } from 'util'
 
 const BotCard = dynamic(() => import('@components/BotCard'), { ssr: true })
 const ServerCard = dynamic(() => import('@components/ServerCard'), { ssr: true })
 
 interface UserPageProps {
-  user: {
-    servers: ServerList[];
-    bots: Bot[];
-    id: string;
-    archive_flags: number;
-    username: string;
-    discriminator: string;
-    avatar: string;
-    new: boolean;
-    published_date: Date;
-  }
+  user: User
   error: boolean;
   message: string;
   statusCode: number;
+}
+interface User {
+  servers: ServerList[];
+  bots: Bot[];
+  id: string;
+  archive_flags: number;
+  username: string;
+  discriminator: string;
+  avatar: string;
+  new: boolean;
+  published_date: Date;
 }
 export const getServerSideProps: GetServerSideProps = async(ctx) => {
   let user = (await fetch(`${process.env.API_DOMAIN}/users/${encodeURI(ctx.params.id as string)}`).then(res => res.json())) as any;
@@ -50,11 +52,24 @@ export const getServerSideProps: GetServerSideProps = async(ctx) => {
     }
   };
 };
+
+const userDescription = (user: User) => {
+  if(user.servers.length === 0 && user.bots.length >= 1) {
+    return `${user.bots.map(x => x.name).join(', ')} 봇을 제작합니다.`
+  } else if(user.bots.length === 0 && user.servers.length >= 1) {
+    return `${user.servers.map(x => x.name).join(', ')} 서버를 운영중입니다`
+  } else if(user.servers.length >= 1 && user.bots.length >= 1) {
+    return `${user.servers.map(x => x.name).join(', ')} 서버를 운영하고 ${user.bots.map(x => x.name).join(', ')} 봇을 제작합니다`
+  } else {
+    return `${user.username}님의 아카이브 프로필 입니다`
+  }
+}
+
 const UserPage: NextPage<UserPageProps> = ({user, error, message, statusCode}) => {
   if(error) return <ErrorPage message={message} statusCode={statusCode}/>
   return (
       <>
-        <HeadInfo title={user.username + " 프로필 - 아카이브"} image={userAvaterLink(user)} description={`${user.servers.map(x => x.name).join(', ')} 서버를 운영하고 ${user.bots.map(x => x.name).join(', ')} 봇을 제작합니다.`}/>
+        <HeadInfo title={user.username + " 프로필 - 아카이브"} image={userAvaterLink(user)} description={userDescription(user)}/>
         <div className={styles.container}>
         <div className='flex lg:flex-row lg:justify-between lg:flex-nowarp flex-col items-center justify-center flex-warp mx-4'>
           <div className='flex lg:flex-row flex-col items-center'>
@@ -80,8 +95,8 @@ const UserPage: NextPage<UserPageProps> = ({user, error, message, statusCode}) =
               {user.servers.length === 0 ? (<>
                 <span className='text-2xl'>소유한 서버가 없습니다</span>
               </>) : (<>
-                {user.servers.map(server => (<>
-                  <ServerCard guild={server}/>
+                {user.servers.map((server, index) => (<>
+                  <ServerCard key={index} guild={server}/>
                 </>))}
               </>)
               }
@@ -90,12 +105,12 @@ const UserPage: NextPage<UserPageProps> = ({user, error, message, statusCode}) =
           <div className='mx-4'>
             <span className='text-3xl'>소유중인 봇</span>
             <hr className='my-2'/>
-            <div className='my-14 min-h-[10vh] flex flex-row flex-wrap justify-center'>
+            <div className='my-14 min-h-[20vh] flex flex-row flex-wrap items-center justify-center'>
               {user.bots.length === 0 ? (<>
                   <span className='text-2xl'>소유한 봇이 없습니다</span>
               </>):(<>
-                {user.bots.map(bot => (<>
-                  <BotCard bot={bot}/>
+                {user.bots.map((bot, index) => (<>
+                  <BotCard bot={bot} key={index}/>
                 </>))}
               </>)}
             </div>
